6de8758e823c3432862b1452eecf10bfc2d02952
2814109 <2814109k@gmail.com>
2024-01-23 00:43:54 +0900

    u

--- src/main.rs
+++ src/main.rs
@@ -7,15 +7,23 @@ type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
-    let url = env::var("URL").expect("URL must be set");
+    let url = env::var("GITHUB_API").expect("URL must be set");
     // let url = "https://zipcloud.ibsnet.co.jp/api/search";
     println!("{}", url);
     let client: Client = Client::new();
     let response = client
         .get(url)
-        .query(&[("zipcode", "1000002")])
+        .header(
+            reqwest::header::USER_AGENT,
+            "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion",
+        )
         .send()
         .await?;
+    // let response = client
+    //     .get(url)
+    //     .query(&[("zipcode", "1000002")])
+    //     .send()
+    //     .await?;
     let body = response.text().await?;
     println!("{}", body);
 
