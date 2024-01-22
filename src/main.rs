17dd672ef88e69aaa41ed6abacc38c04a5b8c3c4
2814109 <2814109k@gmail.com>
2024-01-23 00:37:52 +0900

    u

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,14 @@
 use dotenv::dotenv;
 use reqwest::Client;
-// use std::env;
+use std::env;
 
 type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[tokio::main]
 async fn main() -> Result<()> {
     dotenv().ok();
-    // let url = env::var("URL").expect("URL must be set");
-    let url = "https://zipcloud.ibsnet.co.jp/api/search";
+    let url = env::var("URL").expect("URL must be set");
+    // let url = "https://zipcloud.ibsnet.co.jp/api/search";
     println!("{}", url);
     let client: Client = Client::new();
     let response = client
