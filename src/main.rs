2f56789c15919eda77dff3172b567bb9d26a2173
2814109 <2814109k@gmail.com>
2024-01-24 23:51:51 +0900

    u

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,15 @@
 use axum::{response::Html, routing::get, Router};
-// use dotenv::dotenv;
-// use reqwest::Client;
-// use std::env;
 
-// type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
+use dotenv::dotenv;
+use reqwest::Client;
+use std::env;
+
+type Result<T> = std::result::Result<T, Box<dyn std::error::Error>>;
 
 #[tokio::main]
 async fn main() {
     // build our application with a route
-    let app = Router::new().route("/", get("hello"));
+    let app = Router::new().route("/", get(handler));
 
     // run it
     let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
@@ -18,24 +19,25 @@ async fn main() {
     axum::serve(listener, app).await.unwrap();
 }
 
-// async fn handler() -> Result<Html<String>> {
-//     dotenv().ok();
-//     let url = env::var("GITHUB_API").expect("URL must be set");
-//     println!("{}", url);
-//     let client: Client = Client::new();
-//     let response = client
-//         .get(url)
-//         .header(
-//             reqwest::header::USER_AGENT,
-//             "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion",
-//         )
-//         .send()
-//         .await?;
+#[axum::debug_handler]
+async fn handler() -> Result<Html<String>> {
+    dotenv().ok();
+    let url = env::var("GITHUB_API").expect("URL must be set");
+    println!("{}", url);
+    let client: Client = Client::new();
+    let response = client
+        .get(url)
+        .header(
+            reqwest::header::USER_AGENT,
+            "Mozilla/5.0 (platform; rv:geckoversion) Gecko/geckotrail Firefox/firefoxversion",
+        )
+        .send()
+        .await?;
 
-//     let body = response.text().await?;
-//     Ok(Html(body))
-//     // Ok((Html(&body)))
-// }
+    let body = response.text().await?;
+    Ok(Html(body))
+    // Ok((Html(&body)))
+}
 // use dotenv::dotenv;
 // use reqwest::Client;
 // use std::env;
