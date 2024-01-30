130eb431ebd2f452cca7c891ceb93eb9645fe71b
2814109 <2814109k@gmail.com>
2024-01-30 22:41:39 +0900

    u

--- src/main.rs
+++ src/main.rs
@@ -13,7 +13,7 @@ use tower_http::cors::CorsLayer;
 async fn main() {
     // build our application with a route
     let cors = CorsLayer::new()
-        .allow_origin("http://localhost:3000".parse::<HeaderValue>().unwrap())
+        .allow_origin("http://localhost:5173".parse::<HeaderValue>().unwrap())
         .allow_methods([Method::GET, Method::POST, Method::PATCH, Method::DELETE])
         .allow_credentials(true)
         .allow_headers([AUTHORIZATION, ACCEPT, CONTENT_TYPE]);
